4cbd520e5debd9565faee7f23db50d0d29849433
土豆哥 <zhanglp15229380174@163.com>
2023-05-07 09:43:40 +0800

    更换创建vite方式

--- src/globals.d.ts
+++
@@ -1,7 +0,0 @@
-export {};
-declare global {
-  interface Window {
-    eventCenterForAppTools: any;
-    __MICRO_APP_BASE_APPLICATION__: any;
-  }
-}
--- src/views/home/index.vue
+++
@@ -1,3 +0,0 @@
-<scritp setup lang="ts"></scritp>
-<template>子应用首页</template>
-<style scoped></style>
